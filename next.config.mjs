378c80c8c1004f3e8564b57967605164c8b132e8
Rienchy Razak <132239292+MRRzkS@users.noreply.github.com>
2026-09-18 13:55:45 +0700

    chore: configure static Next.js export

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+};
+
+export default nextConfig;
